57f2b7fda8d76f50b8a22531d580a8edd55b004f
lucas-dantas10 <lucas.dantas.nogueira@gmail.com>
2022-05-19 14:40:18 -0300

    criando a pasta services no js

---
+++ app/dist/js/services/negociacoes-service.js
@@ -0,0 +1,13 @@
+import { Negociacao } from "../models/negociacao.js";
+export class NegociacoesService {
+    obterNegociacoesDoDia() {
+        const BASE_URL = "http://localhost:8080/dados";
+        return fetch(BASE_URL)
+            .then((resposta) => resposta.json())
+            .then((dados) => {
+            return dados.map(dadoDeHoje => {
+                return new Negociacao(new Date(), dadoDeHoje.vezes, dadoDeHoje.montante);
+            });
+        });
+    }
+}
